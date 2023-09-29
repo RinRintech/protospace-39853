8dc2bca1ad1913e342f0662872679243f7926a5e
RinRintech <144243719+RinRintech@users.noreply.github.com>
2023-09-29 18:21:14 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :index, :show, :edit, :update, :destroy] #←あとでまとめる
+  resources :prototypes do
+    resources :comments , only: :create
+  end
+  resources :users, only: :show
+
+
+end
